36323e6743c319cd65963f50fd812285e34cf1ef
Simonedambra <simonedambra130102@gmail.com>
2024-07-25 23:29:07 +0200

    upd layouts

--- app/Http/Controllers/admin/ProjectController.php
+++ app/Http/Controllers/admin/ProjectController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Projects;
+
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -12,7 +14,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return view('project.index');
+        $projects= Projects::all();
+        return view('project.index',compact("projects"));
     }
 
     /**
--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -2,23 +2,81 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col mt-4">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                        <a href="{{ route('admin.dashboard') }}">torna alla dashboard</a>
-                    </div>
+        <div class="header-page pb-2 mb-4">
+            <div class=" d-flex justify-content-between align-items-center">
+                <h1>Lista dei posts</h1>
+                <div class="d-flex gap-2">
+                    <a href="{{ route('admin.Project.create') }}" class="btn btn-primary" as="button">Crea nuovo</a>
                 </div>
+
             </div>
         </div>
+
+        @if (session('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+        @endif
+
+        @if (!count($projects))
+            <div class="alert alert-warning">
+                Nessun record trovato
+            </div>
+        @else
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col" class="col">#</th>
+                        <th scope="col" class="col-7">Title</th>
+                        <th scope="col" class="col">Slug</th>
+                        <th scope="col" class="col-2 text-right"></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+
+                    @foreach ($projects as $project)
+                        <tr>
+                            <th scope="row">{{ $project->id }}</th>
+                            <td>
+                                @if ($project->cover_image)
+                                    <a href="#" class="btn btn-sm btn-secondary me-3"><i
+                                            class="fa-solid fa-image"></i></a>
+                                @endif{{ $project->title }}
+                            </td>
+                            <td>{{ $project->slug }}</td>
+                            <td>
+                                <div class="d-flex gap-2 justify-content-end">
+                                    <a href="{{ route('admin.Project.show', $project) }}" as="button"
+                                        class="btn btn-info btn-sm"><i class="fa-solid fa-magnifying-glass"></i></a>
+                                    <a href="{{ route('admin.Project.edit', $project) }}" as="button"
+                                        class="btn btn-warning btn-sm"><i class="fa-solid fa-pencil"></i></a>
+
+                                    <form action="{{ route('admin.Project.destroy', $project) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button class="btn btn-danger  btn-sm"><i class="fa-solid fa-bomb"></i></button>
+
+                                    </form>
+                                    {{-- <a href="{{ route('admin.posts.destroy', $post) }}" as="button" class="btn btn-danger"><i
+                                        class="fa-solid fa-bomb"></i></a> --}}
+
+
+
+                                </div>
+
+                            </td>
+                        </tr>
+                    @endforeach
+
+
+
+
+                </tbody>
+            </table>
+        @endif
+
     </div>
 @endsection
